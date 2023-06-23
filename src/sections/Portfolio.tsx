ca2a4cce948a4a93360f7f476700d9104bbaf22b
MikuniTensai <mikunitensai@gmail.com>
2023-06-23 07:37:53 +0700

    test4

--- src/sections/Portfolio.tsx
+++ src/sections/Portfolio.tsx
@@ -12,7 +12,7 @@ export interface IProject {
 export default function Portfolio() {
   return (
     <section id='portfolio' className='px-0 md:px-4 py-8 lg:px-8 w-full'>
-      <h1 className='text-center text-3xl md:text-4xl font-bold'>Proyek</h1>
+      <h1 className='text-center text-3xl md:text-4xl font-bold'>Portfolio</h1>
       <ul
         role='list'
         className='grid gap-6 mt-8 sm:grid sm:grid-cols-2 sm:gap-6 lg:grid-cols-3 lg:gap-8'
