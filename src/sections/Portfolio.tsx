c63696b3e5ec39f459f376883e5b52ee9c76a4c3
MikuniTensai <mikunitensai@gmail.com>
2023-06-23 07:32:33 +0700

    test1

--- src/sections/Portfolio.tsx
+++ src/sections/Portfolio.tsx
@@ -13,14 +13,14 @@ export default function Portfolio() {
   return (
     <section id='portfolio' className='px-0 md:px-4 py-8 lg:px-8 w-full'>
       <h1 className='text-center text-3xl md:text-4xl font-bold'>Proyek</h1>
-      {/* <ul
+      <ul
         role='list'
         className='grid gap-6 mt-8 sm:grid sm:grid-cols-2 sm:gap-6 lg:grid-cols-3 lg:gap-8'
       >
         {PROJECTS.map((project) => (
           <PortfolioCard key={project.name} {...project} />
         ))}
-      </ul> */}
+      </ul>
     </section>
   )
 }
